57099edb60dbad4381a54ffb3c503968e15c5599
dopemmanuel <148891292+dopemmanuel@users.noreply.github.com>
2024-03-14 18:06:18 +0100

    （；´д｀）ゞ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,23 @@
 package org.pebiblioteca
-
-data class Libro(val id: UUID, val titulo: String, val autor: String, val anioPublicacion: Int, val tematica: String, val estado: String)
-class Catalogo {
-    val elementos = mutableListOf<Libro>()
-
-    fun agregarLibro(libro: Libro) {
-        elementos.add(libro)
-    }
-
-    fun buscarLibrosPorAutor(autor: String): List<Libro> {
-        return elementos.filter { it.autor == autor }
-    }
-
-}
-
-data class Prestamo(val libroId: UUID, val usuarioId: UUID, val fechaPrestamo: Long)
-
-class RegistroPrestamos {
-    private val prestamosActuales = mutableListOf<Prestamo>()
-    private val historialPrestamos = mutableListOf<Prestamo>()
-
-    fun registrarPrestamo(prestamo:Prestamo) {
-        prestamosActuales.add(prestamo)
-        historialPrestamos.add(prestamo)
-    }
-}
 fun main() {
-    
+   val registro = RegistroPrestamos()
+
+  	val usuario1 = Usuario("Juan", 1573)
+  	val usuario2 = Usuario("María", 4358)
+
+  	val libro1 = Libro("El Caballero de la armadura Oxidada", "Robert Fisher")
+  	val libro2 = Libro("Cien años de soledad", "Gabriel García Márquez")
+    val libro3 = Libro("Cybersecurity++", "Andrew Martinez")
+    val libro4 = Libro("Clean code", "Robert C. Martin")
+    val libro5 = Libro("The Art of Computer Programming","Donald Knuth")
+    val libro6 = Libro("Sombra sobre Innsmouth","Hijo-Tomas")
+    val libro7 = Libro("Cuando las luces se apagan","James hopkins")
+    val libro8 = Libro("Hidden in darkenss","unknow")
+    val libro9 = Libro("Eletronica Vol 1","Tobias Mathew")
+    val libro10 = Libro("Eletronica Vol 2","Tobias Mathew")
+
+  	registro.registrarPrestamo(usuario1, libro1)
+  	registro.registrarPrestamo(usuario2, libro2)
+
+  	println(registro.consultarHistorialPorLibro(libro1))
 }
