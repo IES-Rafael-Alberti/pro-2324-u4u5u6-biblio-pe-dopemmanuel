103d6e7115600f30a198304cd94b55595255d6b3
dopemmanuel <148891292+dopemmanuel@users.noreply.github.com>
2024-03-14 17:05:45 +0100

    （；´д｀）ゞ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,30 @@
 package org.pebiblioteca
 
+data class Libro(val id: UUID, val titulo: String, val autor: String, val anioPublicacion: Int, val tematica: String, val estado: String)
+class Catalogo {
+    val elementos = mutableListOf<Libro>()
+
+    fun agregarLibro(libro: Libro) {
+        elementos.add(libro)
+    }
+
+    fun buscarLibrosPorAutor(autor: String): List<Libro> {
+        return elementos.filter { it.autor == autor }
+    }
+
+}
+
+data class Prestamo(val libroId: UUID, val usuarioId: UUID, val fechaPrestamo: Long)
+
+class RegistroPrestamos {
+    private val prestamosActuales = mutableListOf<Prestamo>()
+    private val historialPrestamos = mutableListOf<Prestamo>()
+
+    fun registrarPrestamo(prestamo:Prestamo) {
+        prestamosActuales.add(prestamo)
+        historialPrestamos.add(prestamo)
+    }
+}
 fun main() {
-    println("Hello World!")
+    
 }
